a23f0be0f3707452efa4b4fffb09a255a81e524f
Mr-Gabut <faisalbcts60@gmail.com>
2022-03-08 21:42:24 +0700

    Ikeh

---
+++ sayang.py
@@ -0,0 +1,11 @@
+import os,system,time
+
+def bersih():
+   os.system('clear')
+def sayang():
+   print('Pilih yang mana?')
+   print('[1]. Sayang\n [2]. Enggak')
+   time.sleep(3)
+   print('Makasih')
+bersih()
+sayang()
